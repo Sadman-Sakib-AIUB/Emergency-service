504f52686ab4d854b73477b4cb3bcbbe3e990a09
Sadman-Sakib-AIUB <sadmansakib.aiub@gmail.com>
2025-08-28 17:32:29 +0600

    Added Calling Alert Functionality

--- script.js
+++ script.js
@@ -9,4 +9,42 @@ for (let heart of hearts){
     })
 }
 
+// Call Functionality
+let coin = parseInt(document.getElementById('coin-count').innerText);
+
+let calls = document.querySelectorAll('.call');
+let titles = document.querySelectorAll('.title');
+let numbers = document.querySelectorAll('.number');
+
+let i = 0;
+for ( let call of calls){
+    let title = titles[i];
+    let number = numbers[i];
+
+    if( call && title && number)
+    {
+        call.addEventListener('click',function(){
+            let name = title.innerText;
+            let num = number.innerText;
+
+            if( coin >= 20 ){
+                alert("Calling" + name +":" + num)
+
+                coin = coin - 20;
+                document.getElementById('coin-count').innerText = coin;
+
+            }
+
+            else{
+                alert("Not enough coins. You need at least 20 coins to make a call.")
+            }
+        })
+
+        
+    }
+
+    i++;
+
+}
+
 
